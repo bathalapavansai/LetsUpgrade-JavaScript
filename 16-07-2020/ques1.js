8d289815bc1aa0ede9cdc2bb137db61142dad599
pavan sai bathala <bathalapavansai@gmail.com>
2020-07-16 14:01:30 +0530

    Day3

---
+++ 16-07-2020/ques1.js
@@ -0,0 +1,9 @@
+let a=prompt("Enter value and check in console");
+console.log(a);
+function fun()
+{
+    let v=document.getElementById("inputText").value;
+    console.log(v);
+    
+    v==0?document.getElementById("output").innerHTML='Please type Something':document.getElementById("output").innerHTML=`You have typed : ${v}`;
+}
